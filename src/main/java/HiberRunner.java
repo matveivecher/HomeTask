707eadd1b7b32fe274536caa93f9fc2b5f4e8278
Matvei <vecher023@gmail.com>
2021-05-30 13:22:13 +0300

    Hibernate

--- src/main/java/HiberRunner.java
+++ src/main/java/HiberRunner.java
@@ -1,3 +1,4 @@
+import pojos.HibernateUtil;
 import pojos.Person;
 
 import javax.persistence.EntityManager;
@@ -7,7 +8,7 @@ public class HiberRunner {
 
     public static void main(String[] args) {
         Person person=new Person(null,"Matvei","Vecher",18);
-        EntityManager em=HibernateUtil.getEm();
+        EntityManager em= HibernateUtil.getEm();
         em.getTransaction().begin();
         em.persist(person);
         em.getTransaction().commit();
